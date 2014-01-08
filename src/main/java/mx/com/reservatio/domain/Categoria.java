2a2878a63372e3b8bd5779ace5dcbf7878e9726b
amazel <hlezama.neri@gmail.com>
2014-01-08 16:36:07 -0600

    Entities: Categoria Complemento Consultorio Usuario
    Signed-off-by: amazel <hlezama.neri@gmail.com>

---
+++ src/main/java/mx/com/reservatio/domain/Categoria.java
@@ -0,0 +1,46 @@
+package mx.com.reservatio.domain;
+
+import static javax.persistence.GenerationType.AUTO;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+	private Integer idCategoria;
+	private String nombre;
+	private String descripcion;
+
+	@Id
+	@GeneratedValue(strategy = AUTO)
+	public Integer getIdCategoria() {
+		return idCategoria;
+	}
+
+	public void setIdCategoria(Integer idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+
+	@Column(name = "nombre")
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Column(name = "descripcion")
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+}
---
+++ src/main/java/mx/com/reservatio/domain/Usuario.java
@@ -0,0 +1,86 @@
+package mx.com.reservatio.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import static javax.persistence.GenerationType.AUTO;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+	private Integer idUsuario;
+	private String nombre;
+	private String apellido;
+	private String tipo;
+	private String telefono;
+	private String celular;
+	private String contrasena;
+
+	@Id
+	@GeneratedValue(strategy = AUTO)
+	public Integer getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Integer idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	@Column(name = "nombre")
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Column(name = "apellido")
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	@Column(name = "tipo")
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	@Column(name = "telefono")
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	@Column(name = "celular")
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+	@Column(name = "contrasena")
+	public String getContrasena() {
+		return contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+
+}
